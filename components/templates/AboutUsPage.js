870b629fe10ed387b4e77bddd6c3d4ed97674b0c
Amirmohammadkh <amir.m.kh.4883@gmail.com>
2023-05-30 15:46:25 +0330

    adding liara.yaml

--- components/templates/AboutUsPage.js
+++ components/templates/AboutUsPage.js
@@ -12,10 +12,10 @@ const AboutUsPage = () => {
         var xhttp = new XMLHttpRequest();
         xhttp.onreadystatechange = function (data) {
             if (this.readyState == 4 && this.status == 200) {
+                setIsLoading(false)
                 if (document.querySelectorAll("#ienebox6-container")[0]) {
                     setComponentContent(this.response);
                 }
-                setIsLoading(false)
             }
         };
         xhttp.open("GET", "https://iene.ir/iene-box/6/6248cdc5-f653-4a80-9c3b-e77cd8601527/", true);
@@ -55,9 +55,9 @@ const AboutUsPage = () => {
                     </div>
                     {
                         isLoading ? (
-                            <Skeleton animation="wave" variant="rounded" width={210} height={60} />
+                            <Skeleton animation="wave" variant="rounded" width={210} height={60}/>
                         ) : (
-                            <div id="ienebox6-container" dangerouslySetInnerHTML={{ __html: componentContent }} />
+                            <div id="ienebox6-container" dangerouslySetInnerHTML={{__html: componentContent}}/>
                         )
                     }
                 </div>
@@ -69,15 +69,22 @@ const AboutUsPage = () => {
                 </div>
             </div>
             <div>
-                <div className="flex flex-col gap-10 p-5 leading-8 border-[1.7px] border-gray-400 rounded-xl text-justify text-gray-900 font-[500]">
+                <div
+                    className="flex flex-col gap-10 p-5 leading-8 border-[1.7px] border-gray-400 rounded-xl text-justify text-gray-900 font-[500]">
                     <p>
-                        فروشگاه بی بی نبات، از بزرگترین و بهترین فروشگاه های عرضه کننده انواع محصولات یزدی از قبیل نبات و قند به شمار آمده و شرایط آسان و بسیار خوبی را برای خرید این محصولات، در اختیار شما عزیزان قرار خواهد داد.
+                        فروشگاه بی بی نبات، از بزرگترین و بهترین فروشگاه های عرضه کننده انواع محصولات یزدی از قبیل نبات
+                        و قند به شمار آمده و شرایط آسان و بسیار خوبی را برای خرید این محصولات، در اختیار شما عزیزان قرار
+                        خواهد داد.
                     </p>
                     <p>
-                        حال اگر شما عزیزان هم تصمیم دارید تا به خرید نبات یزدی بپردازید و آن را با بهترین شرایط تهیه نمایید، می توانید از فروشگاه بی بی نبات که انواع نبات یزدی اصل و تازه را ارائه نموده، اقدام کرده و در سریع ترین زمان به تهیه این محصول بپردازید.
+                        حال اگر شما عزیزان هم تصمیم دارید تا به خرید نبات یزدی بپردازید و آن را با بهترین شرایط تهیه
+                        نمایید، می توانید از فروشگاه بی بی نبات که انواع نبات یزدی اصل و تازه را ارائه نموده، اقدام کرده
+                        و در سریع ترین زمان به تهیه این محصول بپردازید.
                     </p>
                     <div>
-                        در هر کجای این کشور که باشید، با مراجعه به سایت بی بی نبات می توانید خرید غیر حضوری خود را خیلی سریع و با صرف کمترین هزینه انجام دهید. حتی از این طریق، موفق به خرید پرده نبات زعفرانی یزدی هم خواهید شد؛ این محصول طرفداران زیادی داشته و در نهایت تازگی و کیفیت، تقدیم حضورتان خواهد گشت.
+                        در هر کجای این کشور که باشید، با مراجعه به سایت بی بی نبات می توانید خرید غیر حضوری خود را خیلی
+                        سریع و با صرف کمترین هزینه انجام دهید. حتی از این طریق، موفق به خرید پرده نبات زعفرانی یزدی هم
+                        خواهید شد؛ این محصول طرفداران زیادی داشته و در نهایت تازگی و کیفیت، تقدیم حضورتان خواهد گشت.
                     </div>
                 </div>
             </div>
