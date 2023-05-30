45e6a6b125318e771c04850d83ad28aeba3b2a68
Amirmohammadkh <amir.m.kh.4883@gmail.com>
2023-05-30 15:55:57 +0330

    texting liara cd

--- components/templates/AboutUsPage.js
+++ components/templates/AboutUsPage.js
@@ -25,7 +25,7 @@ const AboutUsPage = () => {
     return (
         <div className="mt-36 md:mt-48 px-3 sm:px-7 lg:px-0 max-w-4xl mx-auto flex flex-col md:flex-row gap-5">
             <div>
-                <h1 className="text-2xl font-bold text-blue-dark text-center md:text-start">درباره بی بی نبات</h1>
+                <h1 className="text-2xl font-bold text-blue-dark text-center md:text-start">سلام درباره بی بی نبات</h1>
                 <div className="flex gap-6 justify-center mt-5">
                     <Link href="https://trustseal.enamad.ir/?id=249865&Code=n7XI910ux5rZG6SjyEa7" target="_blank">
                         <Image src="/images/footerImages/enamad.png" alt="enamad" width={90} height={150}/>
